43b526b08004faa63f95539f8d45e673ff57f3bf
gokhenaltun <gokhan.altun1453@gmail.com>
2022-11-22 20:24:32 +0300

    Metinlerde Sozcuk bulma
    Kullanıcıdan alınan herhangi bir metinde yine kullanıcının aradığı sözcüğün girilen metinde bulunup bulunmadığını söyleyen kod

---
+++ Untitled-3.py
@@ -0,0 +1,7 @@
+
+metin = input("Metin Girin:")
+sozcuk = input("Ulaşmak İstediğin Sözcük: ")
+if sozcuk in metin:
+        print("Sözcük Bu Metinde Mevcut")
+else:
+       print("Sözcük Bu Metinde Mevcut Değil")
